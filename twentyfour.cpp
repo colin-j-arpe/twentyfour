3d97c6619078852b7563df7e9d9e9506d8b2071e
C. Arpe <colin.j.arpe@outlook.com>
2018-10-23 20:53:20 -0400

    Clean unused code from source

--- twentyfour.cpp
+++ twentyfour.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-// #include <string>
 #include <sstream>
 
 bool calculate(int x, int y, int results[]);
 bool createPermutations(int array[], int currentPosition, int lengthOfArray);
 void swapNumbers(int array[], int first_number, int second_number);
 void resetAfterTraversal(int array[], int endPoint);
-void outPut(int array[], int currentPosition, int lengthOfArray);
 bool find24(int numbers[]);
-void moveCounter(int counter[], bool moving_down[], int array_length);
-bool arraysMatch(int first_array[], int second_array[], int array_length);
 void printResults(bool were_the_numbers_swapped[], int first_operation_index, int second_operation_index, int third_operation_index, int nums[], int first_result, int first_result_or_last_num, int last_num_or_first_result, int second_result);
 
 const char operations[4] = {'+', '-', '*', '/'};
@@ -20,16 +16,6 @@ bool solutionHasBeenFound = false;
 int main(int argc, char* argv[]) {
     const int SIZE = argc - 1;
     int nums[SIZE];
-    int counter[SIZE];
-    bool moving_down[SIZE];
-    int send[SIZE];
-
-    //  Fill counter
-    for (int i = 0; i < SIZE; ++i)
-    {
-        counter[i] = i;
-        moving_down[i] = true;
-    }
 
     //  Parse input
     for (int i = 1; i < argc; ++i)
@@ -55,18 +41,6 @@ int main(int argc, char* argv[]) {
         }
     }
 
-
-    // do   {
-    //     moveCounter(counter, moving_down, SIZE);
-    //     for (int i = 0; i < SIZE; ++i)
-    //     {
-    //         send[i] = nums[counter[i]];
-    //     }
-    //     if (find24(send))   {
-    //         return 0;
-    //     }
-    // }   while (!arraysMatch(nums, send, SIZE));
-
     if (createPermutations(nums, 1, SIZE)) {
         return 0;
     }   else    {
@@ -78,41 +52,28 @@ int main(int argc, char* argv[]) {
 }
 
 bool createPermutations(int array[], int position, int length) {
-// std::cout << "in at pos " << position << "\n";
     if (position == length)
     {
-// std::cout << "returning; " << position << " = " << length << "\n";
         return false;
     }
     if (solutionHasBeenFound)
     {
         return true;
     }
-//     if (position < length)
-//     {
-//         createPermutations(array, position + 1, length);
-// // std::cout << "found a match\n";
-// //         return true;
-//     }
     for (int i = position; i >= 0; --i)
     {
-        // find24(array);
         createPermutations(array, position + 1, length);
         if (position == length - 1)
         {
             if (find24(array))
             {
                 return solutionHasBeenFound = true;
-                // return true;
             }
-        // outPut(array, i, length);
-            /* code */
         }
         if (i > 0)
         {
             swapNumbers(array, i, i - 1);
         }   else    {
-        // outPut(array, i, length);
             resetAfterTraversal(array, position);
         }
     }
@@ -127,28 +88,9 @@ void resetAfterTraversal(int array[], int endPoint) {
     array[endPoint] = temp;
 }
 
-void outPut(int array[], int pos, int size)   {
-    std::cout << "count " << ++count << ", position " << pos << ": ";
-    for (int i = 0; i < size; ++i)
-    {
-        std::cout << array[i];
-        if (i < size - 1)
-        {
-            std::cout << ", ";
-        }   else    {
-            std::cout << "\n";
-        }
-    }
-}
-
 void swapNumbers(int array[], int x, int y) {
     int temp = array[x]; array[x] = array[y]; array[y] = temp;
 }
-
-// void moveCounter(int counter[], int moving_down[], SIZE)    {
-//     int temp;
-
-// }
 
 bool calculate (int x, int y, int array[])  {
     int temp;
@@ -176,25 +118,12 @@ bool calculate (int x, int y, int array[])  {
     return swapped;
 }
 
-bool arraysMatch(int arr1[], int arr2[], int len)    {
-    for (int i = 0; i < len; ++i)
-    {
-        if (arr1[i] != arr2[i])
-        {
-            return false;
-        }
-        return true;
-    }
-}
-
 bool find24(int nums[])   {
     int first_results[4];
     int second_results[4];
     int third_results[4];
     int skip_quotient;
     bool swapped[3];
-
-// std::cout << "fired; count is " << ++count << "\n";
 
     swapped[0] = calculate(nums[0], nums[1], first_results);
     skip_quotient = first_results[3] < 0 ? first_results[3] : 0;
